2c573a61774a763914a48d3880c228afde95cdb7
Thanos Koutoulas <koutoulastha@hotmail.com>
2018-10-14 23:54:46 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GitReferenceTest;
+
+namespace GitUsingRefProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Print();
+            a.SomeText();
+        }
+    }
+}
